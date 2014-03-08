3f14e45548fbf0cd05b32a30aa709c24bcf79ef1
Jeffrey Ling <jeff.ling@utoronto.ca>
2014-03-08 02:12:03 -0800

    added lead_status and opportunity_status endpoints

--- test/close.io.js
+++ test/close.io.js
@@ -1,9 +1,13 @@
 var Closeio = require('../lib/close.io.js');
 var config = require('../config.json');
+var assert = require('assert')
+
+function randomString() {
+    return Math.floor(Math.random() * 10000).toString();
+}
 
 describe('Close.io API', function() {
   it('should create, read, updated, delete and search for leads.', function(done) {
-    this.timeout(10000);
     var closeio = new Closeio(config.api_key);
     closeio.lead.create({name: "John Wehr"}).then(function(data){
       return closeio.lead.read(data.id);
@@ -11,20 +15,55 @@ describe('Close.io API', function() {
       return closeio.lead.update(data.id, {name: "John Wehr 2"});
     }).then(function(data){
       return closeio.lead.delete(data.id);
-    }).then(function(data){ 
+    }).then(function(data){
       return closeio.lead.search({name:"Wayne"});
     }).then(function(data){
       done();
     }, function(){
-      throw Error("Could not updated leads.");
+      throw Error(err.error);
     });
   });
+
   it('should throw a verbose error', function(done) {
     var closeio = new Closeio(config.api_key);
     closeio.lead.create({contacts:[{emails:[{email:"test@example.com"}]}]}).then(function(data){
     }).then(function(data){
     }, function(err){
       done();
+    });
+  });
+
+  it('should create, read, update, delete lead statuses', function(done) {
+    var closeio = new Closeio(config.api_key);
+    var randomVal = randomString(); // for confirming update
+    closeio.lead_status.create({label: randomString()}).then(function(data) {
+      return closeio.lead_status.read(data.id);
+    }).then(function(data) {
+      return closeio.lead_status.update(data.id, {label: randomVal});
+    }).then(function(data) {
+      assert(data.label == randomVal)
+      return closeio.lead_status.delete(data.id);
+    }).then(function(data) {
+      return done();
+    }, function(err) {
+      throw Error(err.error);
+    });
+  });
+
+  it('should create, read, update, delete opportunity statuses', function(done) {
+    var closeio = new Closeio(config.api_key);
+    var randomVal = randomString(); // for confirming update
+    closeio.opportunity_status.create({label: randomString()}).then(function(data) {
+      return closeio.opportunity_status.read(data.id);
+    }).then(function(data) {
+      return closeio.opportunity_status.update(data.id, {label: randomVal});
+    }).then(function(data) {
+      assert(data.label == randomVal)
+      return closeio.opportunity_status.delete(data.id);
+    }).then(function(data) {
+      return done();
+    }, function(err) {
+      throw Error(err.error);
     });
   });
 });
