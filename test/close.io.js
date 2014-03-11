15fb488ef62342e573631c847a4629f411f1e6a2
Jeffrey Ling <jeff.ling@utoronto.ca>
2014-03-11 00:28:47 -0700

    ran this through jshint with airbnb settings just to see what it would be like.

--- test/close.io.js
+++ test/close.io.js
@@ -1,90 +1,93 @@
+'use strict';
 var Closeio = require('../lib/close.io.js'),
   config = require('../config.json'),
-  assert = require('assert')
+  assert = require('assert');
 
-  function randomString() {
-    return Math.floor(Math.random() * 10000).toString();
-  }
+function randomString() {
+  return Math.floor(Math.random() * 10000).toString();
+}
 
-describe('Close.io API', function() {
-  it('should create, read, updated, delete and search for leads.', function(done) {
-    var closeio = new Closeio(config.api_key);
+describe('Close.io API', function () {
+  it('should create, read, updated, delete and search for leads.',
+    function (done) {
+    var closeio = new Closeio(config.apiKey);
     closeio.lead.create({
-      name: "John Wehr"
-    }).then(function(data) {
+      name: 'John Wehr'
+    }).then(function (data) {
       return closeio.lead.read(data.id);
-    }).then(function(data) {
+    }).then(function (data) {
       return closeio.lead.update(data.id, {
-        name: "John Wehr 2"
+        name: 'John Wehr 2'
       });
-    }).then(function(data) {
-      assert(data.name == "John Wehr 2");
+    }).then(function (data) {
+      assert(data.name === 'John Wehr 2');
       return closeio.lead.delete(data.id);
-    }).then(function(data) {
+    }).then(function () {
       return closeio.lead.search({
-        name: "Wayne"
+        name: 'Wayne'
       });
-    }).then(function(data) {
+    }).then(function () {
       done();
-    }, function(err) {
-      throw Error(err.error);
+    }, function (err) {
+      throw new Error(err.error);
     });
   });
 
-  it('should throw a verbose error', function(done) {
-    var closeio = new Closeio(config.api_key);
+  it('should throw a verbose error', function (done) {
+    var closeio = new Closeio(config.apiKey);
     closeio.lead.create({
       contacts: [{
         emails: [{
-          email: "test@example.com"
+          email: 'test@example.com'
         }]
       }]
-    }).then(function(data) {}).then(function(data) {}, function(err) {
+    }).then(function () {}).then(function () {}, function () {
       done();
     });
   });
 
-  it('should create, read, update, delete lead statuses', function(done) {
-    var closeio = new Closeio(config.api_key),
+  it('should create, read, update, delete lead statuses', function (done) {
+    var closeio = new Closeio(config.apiKey),
       randomVal = randomString(); // for confirming update
 
     closeio.status.lead.create({
       label: randomString()
-    }).then(function(data) {
+    }).then(function (data) {
       return closeio.status.lead.read(data.id);
-    }).then(function(data) {
+    }).then(function (data) {
       return closeio.status.lead.update(data.id, {
         label: randomVal
       });
-    }).then(function(data) {
-      assert(data.label == randomVal)
+    }).then(function (data) {
+      assert(data.label === randomVal);
       return closeio.status.lead.delete(data.id);
-    }).then(function(data) {
+    }).then(function () {
       return done();
-    }, function(err) {
-      throw Error(err.error);
+    }, function (err) {
+      throw new Error(err.error);
     });
   });
 
-  it('should create, read, update, delete opportunity statuses', function(done) {
-    var closeio = new Closeio(config.api_key),
-      randomVal = randomString(); // for confirming update
+  it('should create, read, update, delete opportunity statuses',
+    function (done) {
+      var closeio = new Closeio(config.apiKey),
+        randomVal = randomString(); // for confirming update
 
-    closeio.status.opportunity.create({
-      label: randomString()
-    }).then(function(data) {
-      return closeio.status.opportunity.read(data.id);
-    }).then(function(data) {
-      return closeio.status.opportunity.update(data.id, {
-        label: randomVal
+      closeio.status.opportunity.create({
+        label: randomString()
+      }).then(function (data) {
+        return closeio.status.opportunity.read(data.id);
+      }).then(function (data) {
+        return closeio.status.opportunity.update(data.id, {
+          label: randomVal
+        });
+      }).then(function (data) {
+        assert(data.label === randomVal);
+        return closeio.status.opportunity.delete(data.id);
+      }).then(function () {
+        return done();
+      }, function (err) {
+        throw new Error(err.error);
       });
-    }).then(function(data) {
-      assert(data.label == randomVal)
-      return closeio.status.opportunity.delete(data.id);
-    }).then(function(data) {
-      return done();
-    }, function(err) {
-      throw Error(err.error);
     });
-  });
 });
